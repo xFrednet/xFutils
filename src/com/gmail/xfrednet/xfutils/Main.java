7b2d896f9ba2940e124345340c8ae18827cd2f7d
xFrednet <xFrednet@gmail.com>
2018-11-16 09:52:36 +0100

    Worked on the way MenuItems will be added to the PopupMenu

--- src/com/gmail/xfrednet/xfutils/Main.java
+++ src/com/gmail/xfrednet/xfutils/Main.java
@@ -5,23 +5,28 @@ import com.gmail.xfrednet.xfutils.util.logger.FileLogger;
 import com.gmail.xfrednet.xfutils.util.logger.NoLogLogger;
 
 import java.awt.Image;
+import java.awt.MenuContainer;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
 import java.awt.SystemTray;
 import java.awt.TrayIcon;
 import java.io.IOException;
+import java.util.List;
 
 import javax.imageio.ImageIO;
+import javax.swing.MenuElement;
 
 import com.gmail.xfrednet.xfutils.plugin.PluginManager;
 import com.gmail.xfrednet.xfutils.util.Logger;
 
 public class Main {
 	
-	public static Logger logger              = null;
-	private static boolean debugEnabled      = false;
-	private static boolean argPluginsEnabled = false;
-	private static boolean argLinksEnabled   = false;
+	public static Logger  Logger             = null;
+	public static boolean IsDebugEnabled     = false;
+	public static boolean ArePluginsEnabled  = false;
+	public static boolean AreLinksEnabled    = false;
 	
-	// This value should only be used by the terminate function
+	// This value should only be used by the TerminateApp function
 	private static Main instance             = null;
 	
 	// ##########################################
@@ -29,13 +34,13 @@ public class Main {
 	// ##########################################
 	public static void main(String[] args) {
 		if (!ProcessArgs(args)) {
-			logger.endLog();
+			Logger.endLog();
 			return; // ProcessArgs has failed
 		}
 		
-		Main main = new Main();
-		if (!main.init()) {
-			logger.logError("main: Something failed durring the initialize of Main. Goodbye");
+		instance = new Main();
+		if (!instance.init()) {
+			Logger.logError("main: Something failed durring the initialize of Main. Goodbye");
 			System.exit(1); // TODO create predefined errors
 		}
 		
@@ -52,32 +57,32 @@ public class Main {
 		for (String arg : args) {
 			switch (arg) {
 			case "-debug":
-				debugEnabled = true;
+				IsDebugEnabled = true;
 				break;
 			case "-conlog":
-				if (logger != null) {
-					logger.logError("ProcessArgs: \"-conlog\": Only one log option can be selected at a time.");
+				if (Logger != null) {
+					Logger.logError("ProcessArgs: \"-conlog\": Only one log option can be selected at a time.");
 				}
-				logger = new ConsoleLogger(debugEnabled);
-				logger.logInfo("ProcessArgs: The log will be written to the console.");
+				Logger = new ConsoleLogger(IsDebugEnabled);
+				Logger.logInfo("ProcessArgs: The log will be written to the console.");
 				break;
 			case "-filelog":
-				if (logger != null) {
-					logger.logError("ProcessArgs: \"-filelog\": Only one log option can be selected at a time.");
+				if (Logger != null) {
+					Logger.logError("ProcessArgs: \"-filelog\": Only one log option can be selected at a time.");
 				}
-				logger = new FileLogger(debugEnabled);
-				logger.logInfo("ProcessArgs: The log will be written to a file.");
+				Logger = new FileLogger(IsDebugEnabled);
+				Logger.logInfo("ProcessArgs: The log will be written to a file.");
 				break;
 			case "-noplugins":
-				argPluginsEnabled = false;
-				if (logger != null) {
-					logger.logInfo("ProcessArgs: \"-noplugins\": Plugins will be disabled.");
+				ArePluginsEnabled = false;
+				if (Logger != null) {
+					Logger.logInfo("ProcessArgs: \"-noplugins\": Plugins will be disabled.");
 				}
 				break;
 			case "-nolinks":
-				argLinksEnabled = false;
-				if (logger != null) {
-					logger.logInfo("ProcessArgs: \"-nolinks\": Links will be disabled.");
+				AreLinksEnabled = false;
+				if (Logger != null) {
+					Logger.logInfo("ProcessArgs: \"-nolinks\": Links will be disabled.");
 				}
 				break;
 			case "-help":
@@ -95,13 +100,13 @@ public class Main {
 		}
 
 		// logger validation
-		if (logger == null) {
-			logger = new NoLogLogger();
+		if (Logger == null) {
+			Logger = new NoLogLogger();
 		}
 
 		// debug info
-		if (debugEnabled) {
-			logger.logDebugMessage("ProcessArgs: Debugging was enabled!");
+		if (IsDebugEnabled) {
+			Logger.logDebugMessage("ProcessArgs: Debugging was enabled!");
 
 			StringBuilder sb = new StringBuilder();
 			for (String arg : args) {
@@ -109,45 +114,181 @@ public class Main {
 				sb.append(" ");
 			}
 
-			logger.logDebugMessage("ProcessArgs: The following arguments where entered: \"" + sb.toString() + "\"");
+			Logger.logDebugMessage("ProcessArgs: The following arguments where entered: \"" + sb.toString() + "\"");
 		}
 		return true;
 	}
 	private static void TerminateApp() {
-		instance.terminate();
-		logger.endLog();
+		if (instance != null) {
+			instance.terminate();
+			instance = null;
+		}
+		if (Logger != null) {
+			Logger.endLog();
+			Logger = null;
+		}
 	}
 	
 	// ####################################################
 	// # Main Class #
 	// ####################################################
-	TrayIcon trayIcon;
+	private static final int MENU_SECTION_PLUGINS = 1;
+	private static final int MENU_SECTION_LINKS  = 2;
+	private static final int MENU_SECTION_META   = 3;
+	
+	private TrayIcon trayIcon;
+	private PopupMenu trayMenu;
+	private MenuItem menuLabelPlugins;
+	private MenuItem menuLabelLinks;
+	private MenuItem menuLabelMeta;
+	
+	// The PluginManager will only be valid if Main.argPluginsEnabled
+	// is true. So please make sure to check for null before usage.
+	private PluginManager pluginManager;
 	
 	private Main() {
 		this.trayIcon = null;
+		this.trayMenu = null;
+		this.menuLabelPlugins = null;
+		this.menuLabelLinks = null;
+		this.menuLabelMeta = null;
+		
+		this.pluginManager = null;
 	}
 	
+	// ##########################################
+	// # init
+	// ##########################################
 	private boolean init() {
+		// Test if the TrayIcon is support
 		if (!SystemTray.isSupported()) {
-			logger.logError("Main: The current system does not support a system tray.");
+			Logger.logError("Main: The current system does not support a system tray.");
 			return false;
 		}
 		
+		// Create and add TrayIcon
 		try {
-			Image icon = ImageIO.read((ClassLoader.getSystemClassLoader().getResource("icon.png")));
+			Image icon = ImageIO.read(ClassLoader.getSystemClassLoader().getResource("icon.png"));
 			this.trayIcon = new TrayIcon(icon);
 			SystemTray.getSystemTray().add(trayIcon);
 			
-			logger.logInfo("Main.init: Added the TrayIcon to the SystemTray.");
-			return true;
+			Logger.logInfo("Main.init: Added the TrayIcon to the SystemTray.");
 		} catch (Exception e) {
-			logger.logError("Main.init: Something failed during the initialization!", e);
+			Logger.logError("Main.init: Something failed during the initialization!", e);
 			return false;
 		}
+		
+		// Menu init
+		initTrayMenu();
+		
+		// Return le trúe
+		return true;
 	}
-	private void terminate() {
-		SystemTray.getSystemTray().remove(trayIcon);
-		logger.logInfo("Main.terminate: Removed the TrayIcon from the SystemTray");
+	private void initTrayMenu() {
+		this.trayMenu = new PopupMenu();
+		
+		// TODO add settings: showLabels
+		// Plugins section
+		MenuItem pluginsLabel = new MenuItem("Plugins");
+		pluginsLabel.setEnabled(false); // make it a label
+		addMenuItem(pluginsLabel, MENU_SECTION_PLUGINS);
+		this.trayMenu.addSeparator();
+		
+		// Links section
+		MenuItem linksLabel = new MenuItem("Links");
+		linksLabel.setEnabled(false); // make it a label
+		addMenuItem(linksLabel, MENU_SECTION_LINKS);
+		this.trayMenu.addSeparator();
+		
+		// Meta section
+		this.menuLabelMeta = new MenuItem("Meta");
+		this.menuLabelMeta.setEnabled(false);
+		this.trayMenu.add(this.menuLabelMeta);
+		
+		// "Exit"-item
+		MenuItem exitItem = new MenuItem("Exit"); // TODO create language class
+		exitItem.addActionListener(e -> {
+			Logger.logDebugMessage("'Exit'-Item: I was activated!");
+			System.exit(0);
+			Logger.logDebugMessage("'Exit'-Item: heyyyyy");
+		});
+		addMenuItem(exitItem, MENU_SECTION_META);
+		
+		addMenuItem(new MenuItem("S1.0"), 1);
+		addMenuItem(new MenuItem("S2.0"), 2);
+		addMenuItem(new MenuItem("S3.0"), 3);
+		addMenuItem(new MenuItem("S1.1"), 1);
+		addMenuItem(new MenuItem("S1.2"), 1);
+		addMenuItem(new MenuItem("S1.3"), 1);
+		addMenuItem(new MenuItem("S3.1"), 3);
+		addMenuItem(new MenuItem("S3.2"), 3);
+		addMenuItem(new MenuItem("S2.1"), 2);
+		addMenuItem(new MenuItem("S2.2"), 2);
+		addMenuItem(new MenuItem("S2.3"), 2);
+		addMenuItem(new MenuItem("S1.4"), 1);
+		addMenuItem(new MenuItem("S2.4"), 2);
+		addMenuItem(new MenuItem("S3.3"), 3);
+		
+		// Add trayMenu to trayIcon
+		this.trayIcon.setPopupMenu(this.trayMenu);
+		Logger.logDebugMessage("The TrayIcon has a PopupMenu now. (Try it now for free: just 1€)");
+	}
+	private void initPluginManager() {
+		if (!ArePluginsEnabled)
+			return;
+		
+		this.pluginManager = new PluginManager(Logger);
+		
+		this.pluginManager.initPlugins();
+		List<MenuItem> pluginItems = this.pluginManager.getPluginMenuElements();
+		for (MenuItem menuItem : pluginItems) {
+			System.out.print(menuItem.getLabel());
+		}
+		
 	}
 	
+	// ##########################################
+	// # terminate
+	// ##########################################
+	private void terminate() {
+		if (this.pluginManager != null) {
+			this.pluginManager.terminatePlugins();
+			this.pluginManager = null;
+			Logger.logInfo("Main.terminate: Terminated the PluginManager instance.");
+		}
+		
+		this.trayIcon = null;
+		// The TrayIcon will removed automatically by the SystenmTray.
+		// Calling the remove function from the ShutdownHook causes the 
+		// Application to idle until the end of dawn.
+	}
+
+	// ##########################################
+	// # Add MenuItems
+	// ##########################################
+	// This method adds the @MenuItem at the end of the section
+	// A new section starts with a separator
+	private void addMenuItem(MenuItem item, int sectionNo) {
+		int sectionEnd = getSeparatorIndex(sectionNo);
+		this.trayMenu.insert(item, sectionEnd);
+	}
+	private int getSeparatorIndex(int separatorNo) {
+		// Loop through the items
+		int itemCount = this.trayMenu.getItemCount();
+		int separatorCount = 0;
+		for (int index = 0; index < itemCount; index++) {
+			// Check if the menuItem is a Separator (The label is "-" for separators)
+			MenuItem item = this.trayMenu.getItem(index);
+			if (!item.getLabel().equals("-")) {
+				continue;
+			}
+			
+			separatorCount++;
+			if (separatorCount == separatorNo) {
+				return index; // Return the index if the items are the same
+			}
+		}
+		
+		return itemCount;
+	}
 }
